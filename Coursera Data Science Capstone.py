5f0f90fa799cbf7a5a6fcac2d72a34b70f03b1dc
kclark10 <70613023+kclark10@users.noreply.github.com>
2020-09-07 21:05:02 -0700

    Add files via upload

---
+++ Coursera Data Science Capstone.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # This is my Data Science Capstone Project
+
+# In[2]:
+
+
+import pandas as pd
+
+
+# In[3]:
+
+
+import numpy as np
+
+
+# In[4]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
